2b0f567f870631f98eb4b08aeca4e7af00467867
zwithe <zwithe133@gmail.com>
2023-06-10 10:53:17 -0400

    created all 52 cards

---
+++ app.js
@@ -0,0 +1,312 @@
+let AceOfHearts = {
+    name: 'ace',
+    suit: 'hearts',
+    value: '1 or 11', //placeholder probably need an if else here
+    quantity: 6
+}
+let AceOfSpades = {
+    name: 'ace',
+    suit: 'spades',
+    value: '1 or 11', //placeholder probably need an if else here
+    quantity: 6
+}
+let AceOfClubs = {
+    name: 'ace',
+    suit: 'clubs',
+    value: '1 or 11', //placeholder probably need an if else here
+    quantity: 6
+}
+let AceOfDiamonds = {
+    name: 'ace',
+    suit: 'diamonds',
+    value: '1 or 11', //placeholder probably need an if else here
+    quantity: 6
+}
+let twoOfHearts = {
+    name: 'two',
+    suit: 'hearts',
+    value: 2,
+    quantity: 6
+}
+let twoOfSpades = {
+    name: 'two',
+    suit: 'spades',
+    value: 2,
+    quantity: 6
+}
+let twoOfClubs = {
+    name: 'two',
+    suit: 'clubs',
+    value: 2,
+    quantity: 6
+}
+let twoOfDiamonds = {
+    name: 'two',
+    suit: 'diamonds',
+    value: 2,
+    quantity: 6
+}
+let threeOfHearts = {
+    name: 'three',
+    suit: 'hearts',
+    value: 3,
+    quantity: 6
+}
+let threeOfSpades = {
+    name: 'three',
+    suit: 'spades',
+    value: 3,
+    quantity: 6
+}
+let threeOfClubs = {
+    name: 'three',
+    suit: 'clubs',
+    value: 3,
+    quantity: 6
+}
+let threeOfDiamonds = {
+    name: 'three',
+    suit: 'diamonds',
+    value: 3,
+    quantity: 6
+}
+let fourOfHearts = {
+    name: 'four',
+    suit: 'hearts',
+    value: 4,
+    quantity: 6
+}
+let fourOfSpades = {
+    name: 'four',
+    suit: 'spades',
+    value: 4,
+    quantity: 6
+}
+let fourOfClubs = {
+    name: 'four',
+    suit: 'clubs',
+    value: 4,
+    quantity: 6
+}
+let fourOfDiamonds = {
+    name: 'four',
+    suit: 'diamonds',
+    value: 4,
+    quantity: 6
+}
+let fiveOfHearts = {
+    name: 'five',
+    suit: 'hearts',
+    value: 5,
+    quantity: 6
+}
+let fiveOfSpades = {
+    name: 'five',
+    suit: 'spades',
+    value: 5,
+    quantity: 6
+}
+let fiveOfClubs = {
+    name: 'five',
+    suit: 'clubs',
+    value: 5,
+    quantity: 6
+}
+let fiveOfDiamonds = {
+    name: 'five',
+    suit: 'diamonds',
+    value: 5,
+    quantity: 6
+}
+let sixOfHearts = {
+    name: 'six',
+    suit: 'hearts',
+    value: 6,
+    quantity: 6
+}
+let sixOfSpades = {
+    name: 'six',
+    suit: 'spades',
+    value: 6,
+    quantity: 6
+}
+let sixOfClubs = {
+    name: 'six',
+    suit: 'clubs',
+    value: 6,
+    quantity: 6
+}
+let sixOfDiamonds = {
+    name: 'six',
+    suit: 'diamonds',
+    value: 6,
+    quantity: 6
+}
+let sevenOfHearts = {
+    name: 'seven',
+    suit: 'hearts',
+    value: 7,
+    quantity: 6
+}
+let sevenOfSpades = {
+    name: 'seven',
+    suit: 'spades',
+    value: 7,
+    quantity: 6
+}
+let sevenOfClubs = {
+    name: 'seven',
+    suit: 'clubs',
+    value: 7,
+    quantity: 6
+}
+let sevenOfDiamonds = {
+    name: 'seven',
+    suit: 'diamonds',
+    value: 7,
+    quantity: 6
+}
+let eightOfHearts = {
+    name: 'eight',
+    suit: 'hearts',
+    value: 8,
+    quantity: 6
+}
+let eightOfSpades = {
+    name: 'eight',
+    suit: 'spades',
+    value: 8,
+    quantity: 6
+}
+let eightOfClubs = {
+    name: 'eight',
+    suit: 'clubs',
+    value: 8,
+    quantity: 6
+}
+let eightOfDiamonds = {
+    name: 'eight',
+    suit: 'diamonds',
+    value: 8,
+    quantity: 6
+}
+let nineOfHearts = {
+    name: 'nine',
+    suit: 'hearts',
+    value: 9,
+    quantity: 6
+}
+let nineOfSpades = {
+    name: 'nine',
+    suit: 'spades',
+    value: 9,
+    quantity: 6
+}
+let nineOfClubs = {
+    name: 'nine',
+    suit: 'clubs',
+    value: 9,
+    quantity: 6
+}
+let nineOfDiamonds = {
+    name: 'nine',
+    suit: 'diamonds',
+    value: 9,
+    quantity: 6
+}
+let tenOfHearts = {
+    name: 'ten',
+    suit: 'hearts',
+    value: 10,
+    quantity: 6
+}
+let tenOfSpades = {
+    name: 'ten',
+    suit: 'spades',
+    value: 10,
+    quantity: 6
+}
+let tenOfClubs = {
+    name: 'ten',
+    suit: 'clubs',
+    value: 10,
+    quantity: 6
+}
+let tenOfDiamonds = {
+    name: 'ten',
+    suit: 'diamonds',
+    value: 10,
+    quantity: 6
+}
+let jackOfHearts = {
+    name: 'jack',
+    suit: 'hearts',
+    value: 10,
+    quantity: 6
+}
+let jackOfSpades = {
+    name: 'jack',
+    suit: 'spades',
+    value: 10,
+    quantity: 6
+}
+let jackOfClubs = {
+    name: 'jack',
+    suit: 'clubs',
+    value: 10,
+    quantity: 6
+}
+let jackOfDiamonds = {
+    name: 'jack',
+    suit: 'diamonds',
+    value: 10,
+    quantity: 6
+}
+let queenOfHearts = {
+    name: 'queen',
+    suit: 'hearts',
+    value: 10,
+    quantity: 6
+}
+let queenOfSpades = {
+    name: 'queen',
+    suit: 'spades',
+    value: 10,
+    quantity: 6
+}
+let queenOfClubs = {
+    name: 'queen',
+    suit: 'clubs',
+    value: 10,
+    quantity: 6
+}
+let queenOfDiamonds = {
+    name: 'queen',
+    suit: 'diamonds',
+    value: 10,
+    quantity: 6
+}
+let kingOfHearts = {
+    name: 'king',
+    suit: 'hearts',
+    value: 10,
+    quantity: 6
+}
+let kingOfSpades = {
+    name: 'king',
+    suit: 'spades',
+    value: 10,
+    quantity: 6
+}
+let kingOfClubs = {
+    name: 'king',
+    suit: 'clubs',
+    value: 10,
+    quantity: 6
+}
+let kingOfDiamonds = {
+    name: 'king',
+    suit: 'diamonds',
+    value: 10,
+    quantity: 6
+}
